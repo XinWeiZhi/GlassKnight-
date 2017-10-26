f73ed5ca53921c9a8c3c38f2ec16287631551dfb
DuskBalls <rzhang20001121@gmail.com>
2017-10-26 09:16:06 -0600

    mute button

--- player.js
+++ player.js
@@ -1,34 +1,36 @@
 class Player {
-    constructor(x,y) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        
+
         this.position = createVector(this.x, this.y);
         this.attack = 1;
         this.speed = 4.5;
         this.jumpLevel = 1;
+        this.width = 60;
+        this.height = 100;
         //perhaps this.hat / this.armor
     }
-    
+
     move() {
-        
+
     }
-    
+
     show() {
-        
+        rect(this.position.x - this.width / 2, this.position.y + this.width / 2, this.width, this.height )
     }
-    
+
     process() {
-        
+        if (keyIsDown(65)) {
+            player.x -= player.speed;
+        }
+        if (keyIsDown(68)) {
+            player.x += player.speed;
+        }
+
+        if (keyIsDown(32)) {
+            player.jump(player.jumpLevel);
+        }
     }
-    if(keyCode == 65) {
-        player.x -= player.speed;
-    } 
-    if (keyCode == 68) {
-        player.x += player.speed;
-    }
-    
-    if (keyCode == 32) {
-        player.jump(player.jumpLevel); 
-    }
+
 }
